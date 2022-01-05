849c946ad792a724c8c4d3b941e4947d30283804
Admin <khoahoangyahoo@gmail.com>
2022-01-05 08:44:29 +0700

    first commit

---
+++ src/main/java/poly/store/OrganicStoreApplication.java
@@ -0,0 +1,13 @@
+package poly.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganicStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganicStoreApplication.class, args);
+	}
+
+}
